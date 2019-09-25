8e19ffc43dd6ddcddfeb8e8ae39621d0a4a7416d
David Ros <david_rm92@hotmail.es>
2019-09-25 10:41:19 +0200

    SUBIDA DAVID

--- Actividad/src/actividad/actividad2.java
+++ Actividad/src/actividad/actividad2.java
@@ -4,4 +4,33 @@ package actividad;
  */
 public class actividad2 {
 
+    
+    public class hilo1 extends Thread{
+             
+        String linea;
+        @Override
+        public void run() {           
+            StringAux.quitarEspSobrantesV1(linea);
+        }                 
 }
+    
+    public class hilo2 extends Thread{
+   
+        String linea;
+        @Override
+        public void run() {           
+            StringAux.quitarEspSobrantesV2(linea);
+        }                 
+}
+    public class hilo3 extends Thread{
+   
+        String linea;
+        @Override
+        public void run() {           
+            StringAux.quitarEspSobrantesV3(linea);
+        }                 
+}
+    
+}
+
+ 
